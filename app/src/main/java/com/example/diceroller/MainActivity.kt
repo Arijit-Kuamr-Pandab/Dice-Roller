4cee8ce6ee629f2363f1a1e16f0bc83e8e0459f1
Arijitkumarpandab <arijitkumarpandab@gmail.com>
2021-04-21 23:30:59 +0530

    completed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,19 +3,29 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
+    private lateinit var image: ImageView
     private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button = findViewById(R.id.button)
-        textView = findViewById(R.id.diceText)
+        image = findViewById(R.id.diceImage)
         button.setOnClickListener {
             val value: Int = roll()
-            textView.text = value.toString()
+            when(value){
+                1-> Glide.with(this).load(R.drawable.dice_1).placeholder(R.drawable.ic_launcher_background).into(image)
+                2-> Glide.with(this).load(R.drawable.dice_2).placeholder(R.drawable.ic_launcher_background).into(image)
+                3-> Glide.with(this).load(R.drawable.dice_3).placeholder(R.drawable.ic_launcher_background).into(image)
+                4-> Glide.with(this).load(R.drawable.dice_4).placeholder(R.drawable.ic_launcher_background).into(image)
+                5-> Glide.with(this).load(R.drawable.dice_5).placeholder(R.drawable.ic_launcher_background).into(image)
+                6-> Glide.with(this).load(R.drawable.dice_6).placeholder(R.drawable.ic_launcher_background).into(image)
+            }
         }
     }
 
